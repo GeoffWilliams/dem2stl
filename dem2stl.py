f539fad7f546f506329cf9578c918f2d60575f4c
Geoff Williams <geoff@geoffwilliams.me.uk>
2014-01-07 21:49:56 +1100

    initial broken linking to surrounding pixels; use file api to write data

--- dem2stl.py
+++ dem2stl.py
@@ -30,7 +30,7 @@ def main():
             help='filename for output file', required=True)
 
     args = parser.parse_args()
-    convert_height_map(height_map(args.input_file))
+    convert_height_map(height_map(args.input_file), args.output_file)
        
  
        
@@ -56,7 +56,10 @@ def height_map(input_file):
     x_dim, y_dim = im.size
 #    print ("creating map %dx%d" % (x_dim, y_dim))
     values = numpy.empty((x_dim, y_dim))
-    
+ 
+    x_dim=3
+    y_dim=3
+   
     for x in range(0,x_dim):
         for y in range(y_dim -1,0,-1):
             (r,g,b) = pix[x,y]
@@ -67,14 +70,14 @@ def height_map(input_file):
     return values
 
 
-def stl_header():
-    print("solid DEM")
+def stl_header(output_file):
+    output_file.write("solid DEM\n")
     
-def stl_footer():
-    print("endsolid")
+def stl_footer(output_file):
+    output_file.write("endsolid\n")
 
 
-def convert_height_map(height_map):
+def convert_height_map(height_map, output_filename):
     """
     Convert height map to STL data
 
@@ -90,32 +93,91 @@ def convert_height_map(height_map):
 
     ( y axis flipped vs jpeg)
     """
+    output_file = open(output_filename, "w")
 
-    stl_header()    
+    stl_header(output_file)
 
-    for x in range(0,len(height_map)-1):
-        for y in range(0,len(height_map[x])-1):
+    for x in range(1,3):
+#len(height_map) -1):
+        for y in range(1,3):
+#len(height_map[x]) -1):
+            link_pixel(output_file, height_map, x, y)
 
-            # draw a pair of triangles (square) for each pixel equivalent
-            print("facet normal 1 1 1")
-            print("\touter loop")
-            print("\t\tvertex %d %d %d" % (x,y, height_map[x][y]))
-            print("\t\tvertex %d %d %d" % (x+1,y+1, height_map[x+1][y+1]))
-            print("\t\tvertex %d %d %d" % (x,y+1, height_map[x][y+1]))
-            print("\tendloop")
-            print("endfacet")
-                        
-            print("facet normal 1 1 1")
-            print("\touter loop")
-            print("\t\tvertex %d %d %d" % (x,y, height_map[x][y]))
-            print("\t\tvertex %d %d %d" % (x+1,y, height_map[x+1][y]))
-            print("\t\tvertex %d %d %d" % (x+1,y+1, height_map[x+1][y+1]))
-            print("\tendloop")
-            print("endfacet")
+    stl_footer(output_file)
+
+def facet(output_file, triangle, normal="1 1 1"):
+    """
+    write a facet using arrays of coordinates
+    """
+    output_file.write("facet normal %s\n" % (normal))
+    output_file.write("\touter loop\n")
+    output_file.write("\t\tvertex %d %d %d\n" % (triangle["xs"][0], triangle["ys"][0], triangle["zs"][0]))
+    output_file.write("\t\tvertex %d %d %d\n" % (triangle["xs"][1], triangle["ys"][1], triangle["zs"][1]))
+    output_file.write("\t\tvertex %d %d %d\n" % (triangle["xs"][2], triangle["ys"][2], triangle["zs"][2]))
+    output_file.write("\tendloop\n")
+    output_file.write("endfacet\n")
 
 
-    stl_footer()
+def triangle(coords, k1, k2, k3):
+    """
+    construct a map of ordered coordinates
+    """
+    return {
+        "xs": [coords[k1]["x"], coords[k2]["x"], coords[k3]["x"]], 
+        "ys": [coords[k2]["y"], coords[k2]["y"], coords[k3]["y"]],
+        "zs": [coords[k3]["z"], coords[k2]["z"], coords[k3]["z"]],
+    }
 
+
+
+
+def link_pixel(output_file, height_map, x, y):
+    """
+    create 8 triangles from this pixel to the surrounding pixels
+    """
+
+    # compute the coordinates for reference later
+    coords = {
+        "n":    {"x": x,    "y": y+1,   "z": height_map[x][y+1]},
+        "ne":   {"x": x+1,  "y": y+1,   "z": height_map[x+1][y+1]},
+        "e":    {"x": x+1,  "y": y,     "z": height_map[x+1][y]},
+        "se":   {"x": x-1,  "y": y+1,   "z": height_map[x-1][y+1]},
+        "s":    {"x": x,    "y": y-1,   "z": height_map[x][y-1]},
+        "sw":   {"x": x-1,  "y": y-1,   "z": height_map[x-1][y-1]},
+        "w":    {"x": x-1,  "y": y,     "z": height_map[x-1][y]},
+        "nw":   {"x": x-1,  "y": y+1,   "z": height_map[x-1][y+1]},
+        "p":    {"x": x,    "y": y,     "z": height_map[x][y]},
+    }
+
+
+    # North pair
+    # NNW
+    facet(output_file, triangle(coords, "p", "nw", "n"))
+
+    # NNE
+    facet(output_file, triangle(coords, "p", "n", "ne"))
+    
+    # East pair
+    # ENE
+    facet(output_file, triangle(coords, "p", "ne", "e"))
+
+    # ESE
+    facet(output_file, triangle(coords, "p", "e", "se"))
+
+    # South pair
+    # SSW
+    facet(output_file, triangle(coords, "p", "se", "s"))
+
+    # SSE
+    facet(output_file, triangle(coords, "p", "s", "sw"))
+
+    # West pair
+    # WSW
+    facet(output_file, triangle(coords, "p", "sw", "w"))
+
+    # WNW
+    facet(output_file, triangle(coords, "p", "w", "nw"))
+    
 main()
 
 
