c087acf3630c35be8a0d6cb939a83338efa575fa
Geoff Williams <geoff@geoffwilliams.me.uk>
2014-01-08 17:56:57 +1100

    fixed triangle winding, added normal calculation, added pixel scaling

--- dem2stl.py
+++ dem2stl.py
@@ -28,14 +28,25 @@ def main():
             help='filename for input file', required=True)
     parser.add_argument('--output-file', dest='output_file', action='store',
             help='filename for output file', required=True)
+    parser.add_argument('--r-ratio', dest='r_ratio', action='store',
+            help='ratio for red pixels', default=0.05, type=float)
+    parser.add_argument('--g-ratio', dest='g_ratio', action='store',
+            help='ratio for green pixels', default=0.01, type=float)
+    parser.add_argument('--b-ratio', dest='b_ratio', action='store',
+            help='ratio for blue pixels', default=-0.05, type=float)
+    parser.add_argument('--pixel-ratio', dest='pixel_ratio', action='store',
+            help='pixel to world unit ratio', default=1, type=float)
+
 
     args = parser.parse_args()
-    convert_height_map(height_map(args.input_file), args.output_file)
+    convert_height_map(
+        height_map(args.input_file, args.r_ratio, args.g_ratio, args.b_ratio), 
+        args.output_file, args.pixel_ratio)
        
  
        
 
-def height_map(input_file):
+def height_map(input_file, r_ratio, g_ratio, b_ratio):
     """
     Convert the image pixel values to a 2D array of heights
 
@@ -51,21 +62,18 @@ def height_map(input_file):
     """
     im = Image.open(input_file) #Can be many different formats.
     pix = im.load()
-#    print im.size #Get the width and hight of the image for iterating over
+    print im.size #Get the width and hight of the image for iterating over
 
     x_dim, y_dim = im.size
-#    print ("creating map %dx%d" % (x_dim, y_dim))
+    print ("creating map %dx%d" % (x_dim, y_dim))
     values = numpy.empty((x_dim, y_dim))
  
-    x_dim=3
-    y_dim=3
-   
     for x in range(0,x_dim):
         for y in range(y_dim -1,0,-1):
             (r,g,b) = pix[x,y]
             
             # r+g+b = meters * scale factor(???)
-            values[x][y_dim-y] = (r*0.1)+(g*0.01)+(b*-0.1)
+            values[x][y_dim-y] = (r*r_ratio)+(g*g_ratio)+(b*-b_ratio)
 
     return values
 
@@ -77,7 +85,7 @@ def stl_footer(output_file):
     output_file.write("endsolid\n")
 
 
-def convert_height_map(height_map, output_filename):
+def convert_height_map(height_map, output_filename, pixel_ratio):
     """
     Convert height map to STL data
 
@@ -97,19 +105,81 @@ def convert_height_map(height_map, output_filename):
 
     stl_header(output_file)
 
-    for x in range(1,3):
-#len(height_map) -1):
-        for y in range(1,3):
-#len(height_map[x]) -1):
-            link_pixel(output_file, height_map, x, y)
+    # process each inner pixel leaving a 1px border of unprocessed pixels
+    max_x = len(height_map) -1
+    max_y = len(height_map[0]) - 1
+
+    max_x = 3
+    max_y = 3
+
+    for x in range(1, max_x):
+        for y in range(1, max_y):
+            link_pixel(output_file, height_map, pixel_ratio, x, y)
+
+    # link the N, S, E, W sides to zero height (the previously unprocessed border)
+    
+    # N
+    for x in range(1, len(height_map)):
+        link_floor_x(output_file, height_map, x)
+    
+
+    # S
+
+    # E
+
+    # W
+
+    # link the bottom sides to create a solid
+    
 
     stl_footer(output_file)
 
-def facet(output_file, triangle, normal="1 1 1"):
+def link_floor_x(output_file, height_map, x):
+
+    coords = {
+        "n":    {"x": x,    "y": y+1,   "z": height_map[x][y+1]},
+        "ne":   {"x": x+1,  "y": y+1,   "z": height_map[x+1][y+1]},
+        "e":    {"x": x+1,  "y": y,     "z": height_map[x+1][y]},
+        "se":   {"x": x+1,  "y": y-1,   "z": height_map[x+1][y-1]},
+        "s":    {"x": x,    "y": y-1,   "z": height_map[x][y-1]},
+        "sw":   {"x": x-1,  "y": y-1,   "z": height_map[x-1][y-1]},
+        "w":    {"x": x-1,  "y": y,     "z": height_map[x-1][y]},
+        "nw":   {"x": x-1,  "y": y+1,   "z": height_map[x-1][y+1]},
+        "p":    {"x": x,    "y": y,     "z": height_map[x][y]},
+    }
+
+
+    # single triangle
+
+    # double triangles
+#    for (i in range(1,len(coords) - 1):
+        
+    # single triangle
+    
+
+def facet(output_file, triangle):
     """
     write a facet using arrays of coordinates
     """
-    output_file.write("facet normal %s\n" % (normal))
+
+     
+    # calculate triangle normals - see:  http://www.mathsisfun.com/algebra/vectors-cross-product.html
+    ax = triangle["xs"][0] - triangle["xs"][2]
+    ay = triangle["ys"][0] - triangle["xs"][2]
+    az = triangle["zs"][0] - triangle["xs"][2]
+
+    bx = triangle["xs"][0] - triangle["xs"][1]
+    by = triangle["ys"][0] - triangle["ys"][1]
+    bz = triangle["zs"][0] - triangle["zs"][1]
+
+    nx = (ay * bz) - (az * by)
+    ny = (az * bx) - (ax * bz)
+    nz = (ax * by) - (ay * bx)
+
+    # extra credit - normal should be of unit length..
+
+
+    output_file.write("facet normal %d %d %d\n" % (nx, ny, nz))
     output_file.write("\touter loop\n")
     output_file.write("\t\tvertex %d %d %d\n" % (triangle["xs"][0], triangle["ys"][0], triangle["zs"][0]))
     output_file.write("\t\tvertex %d %d %d\n" % (triangle["xs"][1], triangle["ys"][1], triangle["zs"][1]))
@@ -124,59 +194,77 @@ def triangle(coords, k1, k2, k3):
     """
     return {
         "xs": [coords[k1]["x"], coords[k2]["x"], coords[k3]["x"]], 
-        "ys": [coords[k2]["y"], coords[k2]["y"], coords[k3]["y"]],
-        "zs": [coords[k3]["z"], coords[k2]["z"], coords[k3]["z"]],
+        "ys": [coords[k1]["y"], coords[k2]["y"], coords[k3]["y"]],
+        "zs": [coords[k1]["z"], coords[k2]["z"], coords[k3]["z"]],
     }
 
+def scale_pixel(pixel_ratio, n):
+    return pixel_ratio * n
 
 
-
-def link_pixel(output_file, height_map, x, y):
+def link_pixel(output_file, height_map, pixel_ratio, x, y):
     """
     create 8 triangles from this pixel to the surrounding pixels
+    
+    triangles need to be wound counter clockwise
     """
 
     # compute the coordinates for reference later
+    
+    # scaled x - 1
+    sxm1 = scale_pixel(pixel_ratio, x-1)
+    # scaled x
+    sx = scale_pixel(pixel_ratio, x)
+    # scaled x + 1
+    sxp1 = scale_pixel(pixel_ratio, x+1)
+    
+    # scaled y - 1
+    sym1 = scale_pixel(pixel_ratio, y-1)
+    # scaled y
+    sy = scale_pixel(pixel_ratio, y)
+    # scaled y + 1
+    syp1 = scale_pixel(pixel_ratio, y+1)
+
     coords = {
-        "n":    {"x": x,    "y": y+1,   "z": height_map[x][y+1]},
-        "ne":   {"x": x+1,  "y": y+1,   "z": height_map[x+1][y+1]},
-        "e":    {"x": x+1,  "y": y,     "z": height_map[x+1][y]},
-        "se":   {"x": x-1,  "y": y+1,   "z": height_map[x-1][y+1]},
-        "s":    {"x": x,    "y": y-1,   "z": height_map[x][y-1]},
-        "sw":   {"x": x-1,  "y": y-1,   "z": height_map[x-1][y-1]},
-        "w":    {"x": x-1,  "y": y,     "z": height_map[x-1][y]},
-        "nw":   {"x": x-1,  "y": y+1,   "z": height_map[x-1][y+1]},
-        "p":    {"x": x,    "y": y,     "z": height_map[x][y]},
+        "n":    {"x": sx,    "y": syp1,   "z": height_map[x][y+1]},
+        "ne":   {"x": sxp1,  "y": syp1,   "z": height_map[x+1][y+1]},
+        "e":    {"x": sxp1,  "y": sy,     "z": height_map[x+1][y]},
+        "se":   {"x": sxp1,  "y": sym1,   "z": height_map[x+1][y-1]},
+        "s":    {"x": sx,    "y": sym1,   "z": height_map[x][y-1]},
+        "sw":   {"x": sxm1,  "y": sym1,   "z": height_map[x-1][y-1]},
+        "w":    {"x": sxm1,  "y": sy,     "z": height_map[x-1][y]},
+        "nw":   {"x": sxm1,  "y": syp1,   "z": height_map[x-1][y+1]},
+        "p":    {"x": sx,    "y": sy,     "z": height_map[x][y]},
     }
 
 
     # North pair
     # NNW
-    facet(output_file, triangle(coords, "p", "nw", "n"))
+    facet(output_file, triangle(coords, "p", "n", "nw"))
 
     # NNE
-    facet(output_file, triangle(coords, "p", "n", "ne"))
+    facet(output_file, triangle(coords, "p", "ne", "n"))
     
     # East pair
     # ENE
-    facet(output_file, triangle(coords, "p", "ne", "e"))
+    facet(output_file, triangle(coords, "p", "e", "ne"))
 
     # ESE
-    facet(output_file, triangle(coords, "p", "e", "se"))
+    facet(output_file, triangle(coords, "p", "se", "e"))
 
     # South pair
     # SSW
-    facet(output_file, triangle(coords, "p", "se", "s"))
+    facet(output_file, triangle(coords, "p", "sw", "s"))
 
     # SSE
-    facet(output_file, triangle(coords, "p", "s", "sw"))
+    facet(output_file, triangle(coords, "p", "s", "se"))
 
     # West pair
     # WSW
-    facet(output_file, triangle(coords, "p", "sw", "w"))
+    facet(output_file, triangle(coords, "p", "w", "sw"))
 
     # WNW
-    facet(output_file, triangle(coords, "p", "w", "nw"))
+    facet(output_file, triangle(coords, "p", "nw", "w"))
     
 main()
 
